fea8b1cdb6d94705eb4f472adf7ce7fa7e53a3bd
Termax-Romun <124915795+Romun-Termux@users.noreply.github.com>
2023-02-17 01:08:56 +0600

    Create gazi.py

---
+++ gazi.py
@@ -0,0 +1,21 @@
+#Tor real Abbu MHE
+
+#Tor mare chudi DEC kor
+
+#Fuck U 
+
+#এটা মাথাই রাখবি 
+
+#তোর রিয়েল আব্বু জয়
+
+#চোরের বাচ্চা 
+
+#কুততার বাচ্চার শরম নাই
+
+#চোর মাদারচোদ 
+
+import os
+
+import MHE
+
+MHE.login()
